b4a83115ff484377c39bc5c6aa445bb836905526
sachingahlot21 <gahlotsachin69@gmail.com>
2024-08-27 23:48:35 +0530

    f_vault

---
+++ frontend/src/components/Public/Public.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Public() {
+  return (
+    <div>Public
+
+        <Link to={'/'} ></Link>
+    </div>
+  )
+}
+
+export default Public
