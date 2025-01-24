6daeac99f4a30902bfee68ba537709bdba7844d2
sachingahlot21 <gahlotsachin69@gmail.com>
2025-01-24 15:42:33 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,17 @@ const cors = require('cors')
 
 const userRoute = require('./routes/index')
 const app = express()
-
-const PORT = 3000
 app.use(cors())
 
 const corsOptions ={
-    origin:'http://localhost:5174', 
-    credentials:true,            //access-control-allow-credentials:true
+    origin:'*', 
+    credentials:true,            
     optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
+
+const PORT = 3000
+
 app.use(express.json());
 
 const uri = process.env.MONGO_URI;
