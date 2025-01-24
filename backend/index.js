f83d8ccd6558f1d1397d303e6ce28aa756828945
sachingahlot21 <gahlotsachin69@gmail.com>
2025-01-24 17:30:38 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.use('/', userRoute)
+app.get('/', userRoute)
 
 
 app.listen(PORT, () => console.log("APP STARTED AT PORT : ", PORT))
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/signin': 'https://feedback-vault-api.vercel.app',  // Proxy API requests
+    },
+  },
 })
+
