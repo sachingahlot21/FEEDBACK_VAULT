59bbdeb3964aac2e98df27efa8cf7a510600e257
sachingahlot21 <gahlotsachin69@gmail.com>
2025-01-27 11:32:39 +0530

    update frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/signin': 'https://feedback-vault-api.vercel.app',  // Proxy API requests
-    },
-  },
 })
 
